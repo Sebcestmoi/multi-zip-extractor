40e481fc0fd3b8690f9dd34b90cbcc62bbbe7b5a
Sebastien Aubonney <s.aubonney@hotmail.com>
2022-12-10 20:49:17 +0100

    update

--- src/app.py
+++ src/app.py
@@ -56,92 +56,94 @@ class App:
 
         self.finised = tk.StringVar()
 
+        # setting frames
         self.frame_1 = tk.Frame(self.root, bg=self.BG_COLOR)
         self.frame_1.grid(column=0, row=0, padx=10, pady=5, sticky=NSEW)
 
         self.frame_2 = tk.Frame(self.root, bg=self.BG_COLOR)
         self.frame_2.grid(column=1, row=0, padx=10, pady=5, sticky=NSEW)
-        # self.frame_2 = frame(self.root)
 
-        # F1 buttons
-        # setting search button at row 0
+        # Frame 1
+        # Row 0
         self.load_button = tk.Button(
             self.frame_1,
-            text="Select source directory", 
-            command=self.select_files, 
-            bg=self.BT_COLOR, 
+            text="Select source directory",
+            command=self.select_files,
+            bg=self.BT_COLOR,
             fg=self.BG_COLOR
         ).grid(column=0, row=0, sticky=tk.EW, pady=5)
 
-        # setting search button at row 1
+        # Row 1
         self.extract_dir_button = tk.Button(
             self.frame_1,
-            text="Select extraction directory", 
-            command=self.select_extract_directory, 
-            bg=self.BT_COLOR, 
+            text="Select extraction directory",
+            command=self.select_extract_directory,
+            bg=self.BT_COLOR,
             fg=self.BG_COLOR
         ).grid(column=0, row=1, sticky=tk.EW, pady=5)
 
+        # Row 2
         self.extract_button = tk.Button(
             self.frame_1,
-            text="Extract all", 
-            command=self.exctract_file, 
-            bg=self.BT_COLOR, 
+            text="Extract all",
+            command=self.exctract_file,
+            bg=self.BT_COLOR,
             fg=self.BG_COLOR
         ).grid(column=0, row=2, sticky=tk.EW, pady=5)
 
-        # setting quit button at row 3
+        # Row 3
         self.close_button = tk.Button(
             self.frame_1,
-            text="Close", 
-            command=self.root.quit, 
-            bg=self.BT_COLOR, 
+            text="Close",
+            command=self.root.quit,
+            bg=self.BT_COLOR,
             fg=self.BG_COLOR
         ).grid(column=0, row=3, sticky=tk.EW, pady=5)
  
-        # F2 text and textvariable
-        
+        # Frame 2
+        # Row 0
         self.label_load_directory = tk.Label(
             self.frame_2,
-            textvariable=self.nbr_of_files, 
-            bg=self.BG_COLOR, 
+            textvariable=self.nbr_of_files,
+            bg=self.BG_COLOR,
             fg=self.FG_COLOR
         ).grid(column=0, row=0, sticky=tk.W, pady=7)
-        
+        # Row 0
         self.label_files = tk.Label(
             self.frame_2,
-            text="file(s) selected", 
-            bg=self.BG_COLOR, 
+            text="file(s) selected",
+            bg=self.BG_COLOR,
             fg=self.FG_COLOR
         ).grid(column=1, row=0, sticky=tk.W, pady=7)
-
+        # Row 1
         self.extract_dir_label = tk.Label(
             self.frame_2,
-            textvariable=self.extract_directory, 
-            bg=self.BG_COLOR, 
+            textvariable=self.extract_directory,
+            bg=self.BG_COLOR,
             fg=self.FG_COLOR
         ).grid(columnspan=2, row=1, sticky=tk.W, pady=7)
-
+        # Row 1
         self.extract_label = tk.Label(
             self.frame_2,
-            textvariable=self.nbr_of_processed_files, 
-            bg=self.BG_COLOR, 
+            textvariable=self.nbr_of_processed_files,
+            bg=self.BG_COLOR,
             fg=self.FG_COLOR
         ).grid(column=0, row=2, sticky=tk.W, pady=7)
-        
+        # Row 2        
         self.label_processed_files = tk.Label(
             self.frame_2,
-            text="file(s) unziped", 
-            bg=self.BG_COLOR, 
+            text="file(s) unziped",
+            bg=self.BG_COLOR,
             fg=self.FG_COLOR
         ).grid(column=1, row=2, sticky=tk.W, pady=7)
-
+        # Row 3 -> ToDo add progressbar...
         self.finished_label = tk.Label(
             self.frame_2,
-            textvariable=self.finised, 
-            bg=self.BG_COLOR, 
+            textvariable=self.finised,
+            bg=self.BG_COLOR,
             fg=self.FG_COLOR
         ).grid(columnspan=2, row=3, sticky=tk.W, pady=7)
+
 
     def select_files(self):
         file_type = [('zip file', '*.zip')]
@@ -153,7 +155,7 @@ class App:
         self.nbr_of_files.set(len(path_tuple))
         self.file_list = process_list_of_path(path_tuple)
         self.frame_2.update_idletasks()
-        # print(f"> selected dir: {self.file_list }")
+
 
     def exctract_file(self):
         for file in self.file_list.list:
@@ -163,13 +165,14 @@ class App:
             self.frame_2.update_idletasks()
         self.finised.set(f"Finished! {self.nbr_of_processed_files.get()}/{self.nbr_of_files.get()} processed.")
 
+
     def select_extract_directory(self):
         path = fd.askdirectory(
             title="Load from",
             initialdir=self.path_desktop
         )
-        # print(f"> selected dir: {path}")
         self.extract_directory.set(path)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
