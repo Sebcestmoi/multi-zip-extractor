ce83669bc158428ca4d4a78848aba5f780fe14f9
Sebastien Aubonney <s.aubonney@hotmail.com>
2022-12-12 23:28:26 +0100

    update

--- src/app.py
+++ src/app.py
@@ -2,11 +2,12 @@ import sys
 from random import randrange
 # from tkinter import Tk, Label, Buttontk.
 import tkinter as tk
+from tkinter import *
 from tkinter import EW, N, NSEW, NW, filedialog as fd
 from pathlib import Path
 
-from config.app_cfg import App_color, App_font
-from modules.file_manager import Files, process_list_of_path, unzip_file
+from config.app_cfg import Theme_dark, Theme_wp, Theme_pink, Theme_grey, Theme_psycho, Theme_style, App_font
+from lib.file_manager import Files, process_list_of_path, unzip_file
 
 home = str(Path.home())
 
@@ -22,18 +23,21 @@ else:
     quit()
 
 class App:
-    def __init__(self, root):
+    def __init__(self):
+        
+        self.root = tk.Tk()
+        self.Theme = Theme_wp
         # color settings
-        self.BG_COLOR = App_color.BG_COLOR.value
-        self.FG_COLOR = App_color.FG_COLOR.value
-        self.BT_COLOR = App_color.BT_COLOR.value
-        self.TXT_COLOR = App_color.TXT_COLOR.value
+        self.BG_COLOR = self.Theme.BG_COLOR.value
+        self.FG_COLOR = self.Theme.FG_COLOR.value
+        self.BT_COLOR = self.Theme.BT_COLOR.value
+        self.TXT_BG_COLOR = self.Theme.TXT_BG_COLOR.value
+        self.TXT_FG_COLOR = self.Theme.TXT_FG_COLOR.value
 
         self.TXT_FONT_1 = App_font.TXT_FONT_1.value
         self.TXT_FONT_2 = App_font.TXT_FONT_2.value
 
         # setting GUI
-        self.root = root
         self.root.title("Multi-zip extractor")
         # self.root.geometry("400x200")
         self.root.config(bg=self.BG_COLOR)
@@ -56,22 +60,37 @@ class App:
 
         self.finised = tk.StringVar()
 
-        # setting frames
+        # Menu
+        self.menubar = tk.Menu (self.root, bg=self.BG_COLOR, fg=self.FG_COLOR, tearoff=0)
+        self.filemenu = tk.Menu(self.menubar, bg=self.BG_COLOR, fg=self.FG_COLOR, tearoff=0)
+        self.filemenu.add_command(label="White-purple", command=self.theme_wp)
+        self.filemenu.add_command(label="Dark", command=self.theme_dark)
+        self.filemenu.add_command(label="Pink", command=self.theme_pink)
+        self.filemenu.add_command(label="Grey", command=self.theme_grey)
+        self.filemenu.add_command(label="Psycho", command=self.theme_psycho)
+        self.filemenu.add_command(label="Style", command=self.theme_style)
+
+        self.filemenu.add_separator()
+
+        self.filemenu.add_command(label="Exit", command=self.root.quit)
+        self.menubar.add_cascade(label="Color", menu=self.filemenu)
+
+        self.root.config(menu=self.menubar)
+
+        # setting frame 1
         self.frame_1 = tk.Frame(self.root, bg=self.BG_COLOR)
         self.frame_1.grid(column=0, row=0, padx=10, pady=5, sticky=NSEW)
 
-        self.frame_2 = tk.Frame(self.root, bg=self.BG_COLOR)
-        self.frame_2.grid(column=1, row=0, padx=10, pady=5, sticky=NSEW)
-
         # Frame 1
         # Row 0
-        self.load_button = tk.Button(
+        self.import_dir_button = tk.Button(
             self.frame_1,
             text="Select source directory",
             command=self.select_files,
             bg=self.BT_COLOR,
-            fg=self.BG_COLOR
-        ).grid(column=0, row=0, sticky=tk.EW, pady=5)
+            fg=self.TXT_BG_COLOR
+        )
+        self.import_dir_button.grid(column=0, row=0, sticky=tk.EW, pady=5)
 
         # Row 1
         self.extract_dir_button = tk.Button(
@@ -79,8 +98,9 @@ class App:
             text="Select extraction directory",
             command=self.select_extract_directory,
             bg=self.BT_COLOR,
-            fg=self.BG_COLOR
-        ).grid(column=0, row=1, sticky=tk.EW, pady=5)
+            fg=self.TXT_BG_COLOR
+        )
+        self.extract_dir_button.grid(column=0, row=1, sticky=tk.EW, pady=5)
 
         # Row 2
         self.extract_button = tk.Button(
@@ -88,8 +108,9 @@ class App:
             text="Extract all",
             command=self.exctract_file,
             bg=self.BT_COLOR,
-            fg=self.BG_COLOR
-        ).grid(column=0, row=2, sticky=tk.EW, pady=5)
+            fg=self.TXT_BG_COLOR
+        )
+        self.extract_button.grid(column=0, row=2, sticky=tk.EW, pady=5)
 
         # Row 3
         self.close_button = tk.Button(
@@ -97,52 +118,65 @@ class App:
             text="Close",
             command=self.root.quit,
             bg=self.BT_COLOR,
-            fg=self.BG_COLOR
-        ).grid(column=0, row=3, sticky=tk.EW, pady=5)
+            fg=self.TXT_BG_COLOR,
+        )
+        self.close_button.grid(column=0, row=3, sticky=tk.EW, pady=5)
  
-        # Frame 2
+
+        # Setting frame 2
+        self.frame_2 = tk.Frame(self.root, bg=self.BG_COLOR)
+        self.frame_2.grid(column=1, row=0, padx=10, pady=5, sticky=NSEW)
+
         # Row 0
-        self.label_load_directory = tk.Label(
+        self.label_nbr_imported_file = tk.Label(
             self.frame_2,
             textvariable=self.nbr_of_files,
             bg=self.BG_COLOR,
-            fg=self.FG_COLOR
-        ).grid(column=0, row=0, sticky=tk.W, pady=7)
+            fg=self.TXT_FG_COLOR
+        )
+        self.label_nbr_imported_file.grid(column=0, row=0, sticky=tk.W, pady=7)
         # Row 0
         self.label_files = tk.Label(
             self.frame_2,
             text="file(s) selected",
             bg=self.BG_COLOR,
-            fg=self.FG_COLOR
-        ).grid(column=1, row=0, sticky=tk.W, pady=7)
+            fg=self.TXT_FG_COLOR
+        )
+        self.label_files.grid(column=1, row=0, sticky=tk.W, pady=7)
         # Row 1
         self.extract_dir_label = tk.Label(
             self.frame_2,
             textvariable=self.extract_directory,
             bg=self.BG_COLOR,
-            fg=self.FG_COLOR
-        ).grid(columnspan=2, row=1, sticky=tk.W, pady=7)
+            fg=self.TXT_FG_COLOR
+        )
+        self.extract_dir_label.grid(columnspan=2, row=1, sticky=tk.W, pady=7)
         # Row 1
         self.extract_label = tk.Label(
             self.frame_2,
             textvariable=self.nbr_of_processed_files,
             bg=self.BG_COLOR,
-            fg=self.FG_COLOR
-        ).grid(column=0, row=2, sticky=tk.W, pady=7)
+            fg=self.TXT_FG_COLOR
+        )
+        self.extract_label.grid(column=0, row=2, sticky=tk.W, pady=7)
         # Row 2        
         self.label_processed_files = tk.Label(
             self.frame_2,
             text="file(s) unziped",
             bg=self.BG_COLOR,
-            fg=self.FG_COLOR
-        ).grid(column=1, row=2, sticky=tk.W, pady=7)
+            fg=self.TXT_FG_COLOR
+        )
+        self.label_processed_files.grid(column=1, row=2, sticky=tk.W, pady=7)
         # Row 3 -> ToDo add progressbar...
         self.finished_label = tk.Label(
             self.frame_2,
             textvariable=self.finised,
             bg=self.BG_COLOR,
-            fg=self.FG_COLOR
-        ).grid(columnspan=2, row=3, sticky=tk.W, pady=7)
+            fg=self.TXT_FG_COLOR
+        )
+        self.finished_label.grid(columnspan=2, row=3, sticky=tk.W, pady=7)
+
+        self.root.mainloop()
 
 
     def select_files(self):
@@ -174,7 +208,106 @@ class App:
         self.extract_directory.set(path)
 
 
+    def theme_dark(self):
+        self.Theme = Theme_dark
+        self.apply_theme()
+
+    def theme_pink(self):
+        self.Theme = Theme_pink
+        self.apply_theme()
+
+    def theme_wp(self):
+        self.Theme = Theme_wp
+        self.apply_theme()
+
+    def theme_grey(self):
+        self.Theme = Theme_grey
+        self.apply_theme()
+
+    def theme_psycho(self):
+        self.Theme = Theme_psycho
+        self.apply_theme()
+
+    def theme_style(self):
+        self.Theme = Theme_style
+        self.apply_theme()
+
+
+    def apply_theme(self):
+        
+        self.root.config(
+            bg=self.Theme.BG_COLOR.value
+        )
+
+        self.menubar.config(
+            bg=self.Theme.BG_COLOR.value,
+            fg=self.Theme.FG_COLOR.value
+        )
+        
+        self.filemenu.config(
+            bg=self.Theme.BG_COLOR.value,
+            fg=self.Theme.FG_COLOR.value
+        )
+
+        self.frame_1.config(
+            bg=self.Theme.BG_COLOR.value
+        )
+
+        self.import_dir_button.config(
+            bg=self.Theme.BT_COLOR.value,
+            fg=self.Theme.TXT_BG_COLOR.value
+        )
+
+        self.extract_dir_button.config(
+            bg=self.Theme.BT_COLOR.value,
+            fg=self.Theme.TXT_BG_COLOR.value
+        )  
+
+        self.extract_button.config(
+            bg=self.Theme.BT_COLOR.value,
+            fg=self.Theme.TXT_BG_COLOR.value
+        )
+        
+        self.close_button.config(
+            bg=self.Theme.BT_COLOR.value,
+            fg=self.Theme.TXT_BG_COLOR.value
+        )
+        
+
+        self.frame_2.config(
+            bg=self.Theme.BG_COLOR.value
+        )
+        
+        self.label_nbr_imported_file.config(
+            bg=self.Theme.BG_COLOR.value,
+            fg=self.Theme.TXT_FG_COLOR.value
+        )
+        
+        self.label_files.config(
+            bg=self.Theme.BG_COLOR.value,
+            fg=self.Theme.TXT_FG_COLOR.value
+        )
+                
+        self.extract_dir_label.config(
+            bg=self.Theme.BG_COLOR.value,
+            fg=self.Theme.TXT_FG_COLOR.value
+        )
+                
+        self.extract_label.config(
+            bg=self.Theme.BG_COLOR.value,
+            fg=self.Theme.TXT_FG_COLOR.value
+        )
+                
+        self.label_processed_files.config(
+            bg=self.Theme.BG_COLOR.value,
+            fg=self.Theme.TXT_FG_COLOR.value
+        )
+                
+        self.finished_label.config(
+            bg=self.Theme.BG_COLOR.value,
+            fg=self.Theme.TXT_FG_COLOR.value
+        )
+           
+
 if __name__ == "__main__":
-    root = tk.Tk()
-    my_gui = App(root)
-    root.mainloop()
+    my_gui = App()
--- src/config/app_cfg.py
+++ src/config/app_cfg.py
@@ -1,10 +1,46 @@
 from enum import Enum
 
-class App_color(Enum):
+class Theme_wp(Enum):
     BG_COLOR = "white"
     BT_COLOR = "purple"
     FG_COLOR = "purple"
-    TXT_COLOR = "black"
+    TXT_BG_COLOR = BG_COLOR
+    TXT_FG_COLOR = FG_COLOR
+
+class Theme_grey(Enum):
+    BG_COLOR = "#606060"
+    BT_COLOR = "#CACACA"
+    FG_COLOR = "#CACACA"
+    TXT_BG_COLOR = "black"
+    TXT_FG_COLOR = "white"
+
+class Theme_pink(Enum):
+    BG_COLOR = "#FCEFFB"
+    BT_COLOR = "#F986E2"
+    FG_COLOR = "#F986E2"
+    TXT_BG_COLOR = BG_COLOR
+    TXT_FG_COLOR = FG_COLOR
+
+class Theme_dark(Enum):
+    BG_COLOR = "#000000"
+    BT_COLOR = "#D00000"
+    FG_COLOR = "#D00000"
+    TXT_BG_COLOR = BG_COLOR
+    TXT_FG_COLOR = FG_COLOR
+
+class Theme_psycho(Enum):
+    BG_COLOR = "#FF0000"
+    BT_COLOR = "#00F7FF"
+    FG_COLOR = "#00F7FF"
+    TXT_BG_COLOR = BG_COLOR
+    TXT_FG_COLOR = "black"
+
+class Theme_style(Enum):
+    BG_COLOR = "#E803E8"
+    BT_COLOR = "#06E803"
+    FG_COLOR = "#06E803"
+    TXT_BG_COLOR = "black"
+    TXT_FG_COLOR = "white"
 
 class App_font(Enum):
     TXT_FONT_1 = "Arial 10 bold"
